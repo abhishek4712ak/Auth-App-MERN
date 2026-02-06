78bdffc5391b01a7ecca970f153e60cc83800da0
Abhishek <abhishekak87378@gmail.com>
2026-02-06 10:44:42 +0530

    Configure server proxy in Vite config
    Added server proxy configuration for API requests.

--- auth-app/client/auth-app/vite.config.js
+++ auth-app/client/auth-app/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://auth-app-mern-2.onrender.com', // Your backend server URL
+                changeOrigin: true, // Allows the proxy to change the origin of the request
+                
+            },
+        },
+    }
+});
